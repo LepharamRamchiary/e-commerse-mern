acb46531119a16de733e928878a6ac9632e9c311
Lepha <lepharamchiary@gmail.com>
2024-08-20 13:20:50 +0530

    complet repeat coustomer endpoint

--- api/controllers/customer.controller.js
+++ api/controllers/customer.controller.js
@@ -1,4 +1,5 @@
 import { ShopifyCustomer } from '../models/customer.model.js';
+import { ShopifyOrder } from '../models/order.model.js';
 
 export const getNewCustomersOverTime = async (req, res) => {
     try {
@@ -35,6 +36,62 @@ export const getNewCustomersOverTime = async (req, res) => {
         ]);
 
         res.json(newCustomers);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+
+export const getRepeatCustomersOverTime = async (req, res) => {
+    try {
+        const interval = req.query.interval || 'monthly';
+
+        // Define the grouping based on the interval
+        const groupBy = {
+            daily: {
+                year: { $year: { $toDate: "$created_at" } },
+                month: { $month: { $toDate: "$created_at" } },
+                day: { $dayOfMonth: { $toDate: "$created_at" } }
+            },
+            monthly: {
+                year: { $year: { $toDate: "$created_at" } },
+                month: { $month: { $toDate: "$created_at" } }
+            },
+            quarterly: {
+                year: { $year: { $toDate: "$created_at" } },
+                quarter: { $ceil: { $divide: [{ $month: { $toDate: "$created_at" } }, 3] } }
+            },
+            yearly: {
+                year: { $year: { $toDate: "$created_at" } }
+            }
+        }[interval];
+
+        // Aggregate orders to count purchases per customer
+        const customerPurchases = await ShopifyOrder.aggregate([
+            {
+                $group: {
+                    _id: {
+                        customerId: "$customer.id",
+                        period: groupBy
+                    },
+                    purchaseCount: { $sum: 1 }
+                }
+            },
+            {
+                $match: {
+                    purchaseCount: { $gt: 1 }
+                }
+            },
+            {
+                $group: {
+                    _id: "$_id.period",
+                    repeatCustomersCount: { $sum: 1 }
+                }
+            },
+            { $sort: { "_id.year": 1, "_id.month": 1, "_id.day": 1, "_id.quarter": 1 } }
+        ]);
+
+        res.json(customerPurchases);
     } catch (error) {
         res.status(500).send(error.message);
     }
--- api/routes/customer.routes.js
+++ api/routes/customer.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getNewCustomersOverTime } from "../controllers/customer.controller.js"
+import { getNewCustomersOverTime, getRepeatCustomersOverTime } from "../controllers/customer.controller.js"
 
 const router = express.Router()
 
 router.get('/new-customers-over-time', getNewCustomersOverTime);
+router.get('/repeat-customers-over-time', getRepeatCustomersOverTime);
 
 export default router
