4b940c0b66470275d8a71e0b3c9443609291d4a5
Lepha <lepharamchiary@gmail.com>
2024-08-20 14:05:19 +0530

    completed clv endpoint

--- api/controllers/customer.controller.js
+++ api/controllers/customer.controller.js
@@ -23,13 +23,13 @@ export const getNewCustomersOverTime = async (req, res) => {
         const newCustomers = await ShopifyCustomer.aggregate([
             {
                 $project: {
-                    created_at: { $toDate: "$created_at" } // Ensure the date is in the correct format
+                    created_at: { $toDate: "$created_at" }
                 }
             },
             {
                 $group: {
                     _id: groupBy,
-                    newCustomersCount: { $sum: 1 } // Count the number of new customers
+                    newCustomersCount: { $sum: 1 }
                 }
             },
             { $sort: { "_id.year": 1, "_id.month": 1, "_id.day": 1 } }
@@ -46,7 +46,6 @@ export const getRepeatCustomersOverTime = async (req, res) => {
     try {
         const interval = req.query.interval || 'monthly';
 
-        // Define the grouping based on the interval
         const groupBy = {
             daily: {
                 year: { $year: { $toDate: "$created_at" } },
@@ -66,7 +65,6 @@ export const getRepeatCustomersOverTime = async (req, res) => {
             }
         }[interval];
 
-        // Aggregate orders to count purchases per customer
         const customerPurchases = await ShopifyOrder.aggregate([
             {
                 $group: {
@@ -104,7 +102,7 @@ export const getGeographicalDistribution = async (req, res) => {
             {
                 $group: {
                     _id: "$default_address.city",
-                    count: { $sum: 1 } 
+                    count: { $sum: 1 }
                 }
             },
             {
@@ -115,5 +113,60 @@ export const getGeographicalDistribution = async (req, res) => {
         res.json(cityDistribution);
     } catch (error) {
         res.status(500).send(error.message);
+    }
+};
+
+
+
+export const getCLVByCohorts = async (req, res) => {
+    try {
+        const clvByCohorts = await ShopifyOrder.aggregate([
+            {
+                $addFields: {
+                    createdAtDate: {
+                        $dateFromString: { dateString: "$created_at" }
+                    }
+                }
+            },
+            {
+                $sort: { "customer.id": 1, "createdAtDate": 1 }
+            },
+            {
+                $group: {
+                    _id: "$customer.id",
+                    firstPurchaseDate: { $first: "$createdAtDate" },
+                    totalCLV: { $sum: { $toDouble: "$total_price" } }
+                }
+            },
+            {
+                $group: {
+                    _id: {
+                        year: { $year: "$firstPurchaseDate" },
+                        month: { $month: "$firstPurchaseDate" }
+                    },
+                    cohortCLV: { $sum: "$totalCLV" },
+                    customerCount: { $sum: 1 }
+                }
+            },
+
+            {
+                $sort: { "_id.year": 1, "_id.month": 1 }
+            },
+
+            {
+                $project: {
+                    _id: 0,
+                    year: "$_id.year",
+                    month: "$_id.month",
+                    cohortCLV: 1,
+                    customerCount: 1
+                }
+            }
+        ]);
+
+        res.status(200).json(clvByCohorts);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred while calculating CLV by cohorts' });
     }
 };
--- api/routes/customer.routes.js
+++ api/routes/customer.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getNewCustomersOverTime, getRepeatCustomersOverTime, getGeographicalDistribution } from "../controllers/customer.controller.js"
+import { getNewCustomersOverTime, getRepeatCustomersOverTime, getGeographicalDistribution, getCLVByCohorts } from "../controllers/customer.controller.js"
 
 const router = express.Router()
 
 router.get('/new-customers-over-time', getNewCustomersOverTime);
 router.get('/repeat-customers-over-time', getRepeatCustomersOverTime);
 router.get('/geographical-distribution', getGeographicalDistribution);
+router.get('/clv-by-cohorts', getCLVByCohorts);
 
 export default router
