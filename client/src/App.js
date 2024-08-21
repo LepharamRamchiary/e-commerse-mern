73366370c46eb1256626066b97cf6ee776b17dee
Lepha <lepharamchiary@gmail.com>
2024-08-21 17:04:30 +0530

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import Home from "./pages/Home.jsx"
+import Home from "./pages/Home"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Sales from "./components/Sales.jsx"
-import SalesGrowth from "./components/SalesGrowth.jsx";
-import NewCustomers from "./components/NewCustomers.jsx";
-import RepeatCustomers from "./components/RepeatCustomers.jsx";
-import GeographicalDistribution from "./components/GeographicalDistribution.jsx";
-import Clv from "./components/Clv.jsx";
+import Sales from "./components/Sales"
+import SalesGrowth from "./components/SalesGrowth";
+import NewCustomers from "./components/NewCustomers";
+import RepeatCustomers from "./components/RepeatCustomers";
+import GeographicalDistribution from "./components/GeographicalDistribution";
+import Clv from "./components/Clv";
 import './App.css';
 
 function App() {
